0107117d54c8602296762e4597cbbf407a8c45de
cesar <cesar.madera@utec.edu.pe>
2020-11-13 15:59:15 -0500

    comentario

--- TernarySearchTree/main.cpp
+++ TernarySearchTree/main.cpp
@@ -7,6 +7,9 @@ int main(){
     for (auto it:tree.find("mbutle.11")){
         cout<<it<<endl;
     }
+    //al delete se le pasa el archivo fisico y el nombre del archivo, ya que pueden existir 
+    //varios archivos con el mismo nombre en diferentes lugares. En ese sentido, solo podriamos necesitar eliminar
+    // una de esas repeticiones 
     tree.Delete("faces94/female/mbutle/mbutle.11.jpg","mbutle.11");
     cout<<endl<<endl<<endl;
     cout<<"new result"<<endl;
